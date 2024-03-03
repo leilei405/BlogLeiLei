3b24af03b82d2dcc0745a0ed1723b27bfcd55cef
fll <fllning@163.com>
2024-03-03 11:58:01 +0800

    feat: 将数据库封装成工具

---
+++ src/db.js/mysql.js
@@ -0,0 +1,26 @@
+const mysql = require('mysql');
+const { MYSQL_CONFIG } = require('../config/db') // 引入数据库配置
+
+// 创建连接对象
+const connecTion = mysql.createConnection(MYSQL_CONFIG)
+
+// 开始连接
+connecTion.connect();
+
+// 封装统一执行sql的函数
+const exec = (sql) => {
+    const promise = new Promise((resolve, reject) => {
+        connecTion.query(sql, (err, data) => {
+            if (err) {
+                console.log(err, '==err===');
+                reject(err);
+                return
+            }
+            if (data) {
+                console.log(data, '==data===');
+                resolve(data)
+            }
+        })
+    })
+    return promise;
+}
