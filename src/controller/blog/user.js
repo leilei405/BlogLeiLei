cab4c0ca007a37fe95a1406ab2621667b9c9ceff
fll <fllning@163.com>
2024-03-24 22:04:14 +0800

    feat: 日志功能

--- src/controller/blog/user.js
+++ src/controller/blog/user.js
@@ -1,9 +1,12 @@
 const { exec, escape } = require("../../db/mysql");
+const { genPassword } = require("../../utils/cryp");
 const login = (username, password) => {
   // 假数据使用
   username = escape(username);
+  password = genPassword(password);
   password = escape(password);
   let sql = `select username, realname from bloguser where username=${username} and password=${password}`;
+  console.log(sql, "===sql");
   return exec(sql).then((rows) => {
     return rows[0] || {};
   });
